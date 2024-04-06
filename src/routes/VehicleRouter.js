b806a7d43fa6147f8d7d59b188af39f94ed34f09
goncalos10 <a18847@alunos.ipca.pt>
2024-04-06 11:25:28 +0100

    chore: updated vehiclerouter

--- src/routes/VehicleRouter.js
+++ src/routes/VehicleRouter.js
@@ -2,12 +2,12 @@ const AuthServiceMiddleware = require('../middlewares/AuthServiceMiddleware')
 const roles = require('../entities/Roles')
 const router = require('express').Router()
 
-router.post('/register', AuthServiceMiddleware.execute([roles.ADMIN, roles.MANAGER]), async (req, res) => {
+router.post('/', AuthServiceMiddleware.execute([roles.ADMIN, roles.MANAGER]), async (req, res) => {
     const controller = req.app.get('RegisterVehicleController')
     controller.execute(req, res)
 })
 
-router.get('/get/:vehicleId', async (req, res) => {
+router.get('/:vehicleId', async (req, res) => {
     const controller = req.app.get('GetVehicleDetailsController')
     controller.execute(req, res)
 })
@@ -22,37 +22,37 @@ router.put('/edit', AuthServiceMiddleware.execute([roles.ADMIN, roles.MANAGER]),
     controller.execute(req, res)
 })
 
-router.delete('/delete/:vehicleid', AuthServiceMiddleware.execute([roles.ADMIN, roles.MANAGER]), async (req, res) => {
+router.delete('/:vehicleid', AuthServiceMiddleware.execute([roles.ADMIN, roles.MANAGER]), async (req, res) => {
     const controller = req.app.get('DeleteVehicleController')
     controller.execute(req, res)
 })
 
-router.post('/brands/register', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
+router.post('/brands', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
     const controller = req.app.get('RegisterBrandController')
     controller.execute(req, res)
 })
 
-router.put('/brands/edit', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
+router.put('/brands', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
     const controller = req.app.get('EditBrandController')
     controller.execute(req, res)
 })
 
-router.delete('/brands/delete/:brandid', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
+router.delete('/brands/:brandid', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
     const controller = req.app.get('DeleteBrandController')
     controller.execute(req, res)
 })
 
-router.post('/gastype/register', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
+router.post('/gastypes', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
     const controller = req.app.get('RegisterGasTypeController')
     controller.execute(req, res)
 })
 
-router.put('/gastype/edit', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
+router.put('/gastypes', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
     const controller = req.app.get('EditGasTypeController')
     controller.execute(req, res)
 })
 
-router.delete('/gastype/delete/:gastypeid', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
+router.delete('/gastypes/:gastypeid', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
     const controller = req.app.get('DeleteGasTypeController')
     controller.execute(req, res)
 })
