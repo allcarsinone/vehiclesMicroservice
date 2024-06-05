363266f514307ac7468cd46e03885d804062e267
Hugo Simões <58858094+goncalos10@users.noreply.github.com>
2024-06-05 20:48:12 +0100

    Added vehicleId on edit vehicle

--- src/routes/VehicleRouter.js
+++ src/routes/VehicleRouter.js
@@ -1,60 +1,46 @@
 const AuthServiceMiddleware = require('../middlewares/AuthServiceMiddleware')
 const roles = require('../entities/Roles')
-const router = require('express').Router()
+const vehicleRouter = require('express').Router()
 
-router.post('/', AuthServiceMiddleware.execute([roles.ADMIN, roles.MANAGER]), async (req, res) => {
+
+
+
+
+/**router.post('/', AuthServiceMiddleware.execute([roles.ADMIN, roles.MANAGER]), async (req, res) => {
+    const controller = req.app.get('RegisterVehicleController')
+    controller.execute(req, res)
+})**/
+
+
+vehicleRouter.post('/', async (req, res) => {
     const controller = req.app.get('RegisterVehicleController')
     controller.execute(req, res)
 })
 
-router.get('/:vehicleId', async (req, res) => {
+vehicleRouter.get('/:vehicleid1/:vehicleid2', async (req, res) => {
+    const controller = req.app.get('CompareVehiclesController')
+    controller.execute(req, res)
+})
+
+
+vehicleRouter.get('/:vehicleId', async (req, res) => {
     const controller = req.app.get('GetVehicleDetailsController')
     controller.execute(req, res)
 })
 
-router.get('/', async (req, res) => {
+vehicleRouter.get('/', async (req, res) => {
     const controller = req.app.get('FilterVehiclesController')
     controller.execute(req, res)
 })
 
-router.put('/', async (req, res) => {
+vehicleRouter.put('/:vehicleid', async (req, res) => {
     const controller = req.app.get('EditVehicleController')
     controller.execute(req, res)
 })
 
-router.delete('/:vehicleid', AuthServiceMiddleware.execute([roles.ADMIN, roles.MANAGER]), async (req, res) => {
+vehicleRouter.delete('/:vehicleid', async (req, res) => {
     const controller = req.app.get('DeleteVehicleController')
     controller.execute(req, res)
 })
 
-router.post('/brands', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
-    const controller = req.app.get('RegisterBrandController')
-    controller.execute(req, res)
-})
-
-router.put('/brands', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
-    const controller = req.app.get('EditBrandController')
-    controller.execute(req, res)
-})
-
-router.delete('/brands/:brandid', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
-    const controller = req.app.get('DeleteBrandController')
-    controller.execute(req, res)
-})
-
-router.post('/gastypes', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
-    const controller = req.app.get('RegisterGasTypeController')
-    controller.execute(req, res)
-})
-
-router.put('/gastypes', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
-    const controller = req.app.get('EditGasTypeController')
-    controller.execute(req, res)
-})
-
-router.delete('/gastypes/:gastypeid', AuthServiceMiddleware.execute([roles.ADMIN]), async (req, res) => {
-    const controller = req.app.get('DeleteGasTypeController')
-    controller.execute(req, res)
-})
-
-module.exports = router
+module.exports = vehicleRouter
